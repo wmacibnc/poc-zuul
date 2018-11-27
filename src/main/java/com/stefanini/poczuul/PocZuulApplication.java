82712843734eb05f25e730eb9e2de0a4ebc0ee25
wmcosta <wmcosta@latam.stefanini.com>
2018-11-27 13:07:51 -0200

    banner

--- src/main/java/com/stefanini/poczuul/PocZuulApplication.java
+++ src/main/java/com/stefanini/poczuul/PocZuulApplication.java
@@ -2,10 +2,12 @@ package com.stefanini.poczuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@EnableZuulProxy
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
 public class PocZuulApplication {
 
 	public static void main(String[] args) {
